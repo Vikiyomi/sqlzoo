ff488f2e05bedda8e9ec28b66dc79a81b7b0bb4d
Vikiyomi <43288496+Vikiyomi@users.noreply.github.com>
2020-11-23 12:05:07 +0800

    Update select from world.sql

--- select from world.sql
+++ select from world.sql
@@ -5,3 +5,9 @@
 --United Kingdom has a small population and a small area, it should be excluded.
 select name, population, area from world
 where (area > 3000000) xor (population >250000000)
+
+--9. Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
+--For South America show population in millions and GDP in billions both to 2 decimal places.
+select name, round (population/1000000,2),round(GDP/1000000000,2) 
+from world
+where continent = 'South America'
